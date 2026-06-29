6a52dbdc22f4d07fb2175241de98521bf307b902
sunattah <sundayattah319@gmail.com>
2026-06-29 23:09:25 +0100

    my camel case file

--- practicefolder/camelcasetosnakecase.go
+++ practicefolder/camelcasetosnakecase.go
@@ -1,24 +1,28 @@
-package main
+// if invalid camelCase
+//     return original string
 
-import "strings"
+// result := ""
+
+// for every letter
+//     if uppercase
+//         if not first letter
+//             add "_"
+//         add lowercase version
+//     else
+//         add letter
+
+// return result
+
+package main
 
 func Camelcase(input string) string {
 	if input == "" {
 		return ""
 	}
-	camel_case := strings.Fields(input)
-	for _, ch := range camel_case {
-		if ch != "" {
-			return strings.ToUpper(ch)
-		}
-		if input != camel_case[0] {
+	for i := 0; i < len(input); i++ {
+		c := input[i]
+		if !(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
 			return input
 		}
-		if input == camel_case[0] {
-			return input + "_"
-
-		}
 	}
-	return input
-
 }
