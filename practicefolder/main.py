87c6b7a911bb9835d52d3cc15aa1cc6ebdc37b92
sunattah <sundayattah319@gmail.com>
2026-07-09 16:22:09 +0100

    my first python files

--- practicefolder/main.py
+++ practicefolder/main.py
@@ -17,3 +17,9 @@ def my_function(*args):
   print("All arguments:", args)
 
 my_function("Emil", "Tobias", "Linus")
+
+def my_function(greeting, *names):
+  for name in names:
+    print(greeting, name)
+
+my_function("Hello", "Emil", "Tobias", "Linus")
