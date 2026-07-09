8126ab3b4d1b0e0f65e64eb43b9f802a622a51f7
sunattah <sundayattah319@gmail.com>
2026-07-09 16:18:27 +0100

    my first python files

--- practicefolder/main.py
+++ practicefolder/main.py
@@ -9,3 +9,11 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
