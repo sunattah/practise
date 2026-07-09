74cc1f01328dfbbd4e9cd83d3769032fb2058d7e
sunattah <sundayattah319@gmail.com>
2026-07-09 16:26:19 +0100

    my first python files

--- practicefolder/main.py
+++ practicefolder/main.py
@@ -23,3 +23,13 @@ def my_function(greeting, *names):
     print(greeting, name)
 
 my_function("Hello", "Emil", "Tobias", "Linus")
+
+def my_function(*numbers):
+  total = 0
+  for num in numbers:
+    total += num
+  return total
+
+print(my_function(1, 2, 3))
+print(my_function(10, 20, 30, 40))
+print(my_function(5))
