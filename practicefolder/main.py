81473fd2e822c026258e896d3d371d28f5b3eb4f
sunattah <sundayattah319@gmail.com>
2026-07-09 16:16:29 +0100

    my first python files

--- practicefolder/main.py
+++ practicefolder/main.py
@@ -3,3 +3,9 @@ def my_function(a, b, /, *, c, d):
 
 result = my_function(5, 10, c = 15, d = 20)
 print(result)
+
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
