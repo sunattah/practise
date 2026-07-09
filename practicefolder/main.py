5219b4ea931037aa331198e9c5048cd68cbd43fd
sunattah <sundayattah319@gmail.com>
2026-07-09 16:12:54 +0100

    my first python files

--- practicefolder/main.py
+++ practicefolder/main.py
@@ -1,4 +1,5 @@
-def Myfunc():
-    return 'good day Hello World'
-message = Myfunc()
-print(message)
+def my_function(a, b, /, *, c, d):
+  return a + b + c + d
+
+result = my_function(5, 10, c = 15, d = 20)
+print(result)
