4c62964bd85bce8a293b65764da0270ce4ebfd46
sunattah <sundayattah319@gmail.com>
2026-07-13 16:52:24 +0100

    my praactice on ascii color

--- practicefolder/asciicolor.go
+++ practicefolder/asciicolor.go
@@ -3,7 +3,11 @@ package  main
 import(
 	"fmt"
 )
+func findAll(line, sub string) []int {
+	for 
+
+}
 
 func main() {
-	fmt.Println("\033[32mhello\033[0m", "WOrld")
+	fmt.Println("\033[31mHello", "WOrld")
 }
