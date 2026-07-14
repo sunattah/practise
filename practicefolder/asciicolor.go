67c0b024adf1d6ea58766334f1a96ae6d4323642
sunattah <sundayattah319@gmail.com>
2026-07-14 22:28:41 +0100

    my ascii color

--- practicefolder/asciicolor.go
+++ practicefolder/asciicolor.go
@@ -6,13 +6,21 @@ func findAll(line, sub string) []int {
 	lenLine := len(line)
 	lenSub := len(sub)
 	lenslice := []int{}
-	for i := 0; i <= lenLine- lenSub; i++ {
+	for i := 0; i <= lenLine-lenSub; i++ {
 		if line[i:i+lenSub] == sub {
 			lenslice = append(lenslice, i)
 
 		}
 	}
 	return lenslice
+}
+func markRanges(line, sub string) []bool {
+	indecies := findAll(line, sub)
+	colored := make([]bool, len(line))
+
+	for _, ch := range indecies {
+	}
+
 }
 func main() {
 	fmt.Println(findAll("a king kitten have kit", "kit"))
