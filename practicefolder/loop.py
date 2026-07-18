164660ff193e412a27789755d056172f778032ca
sunattah <sundayattah319@gmail.com>
2026-07-18 17:51:32 +0100

    my python loop

--- practicefolder/loop.py
+++ practicefolder/loop.py
@@ -1,3 +1,5 @@
-# def print_evens():
-for f in range(51):
-  print(f)
+def print_evens(limit):
+    for number in range(51):
+        if number %2 == 0:
+            print(number)
+print(print_evens(44))
