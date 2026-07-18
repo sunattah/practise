fdac44c27af56ab5afae45ae7008b5bcddc5ac71
sunattah <sundayattah319@gmail.com>
2026-07-18 16:51:02 +0100

    my conditions in python

---
+++ practicefolder/condition.py
@@ -0,0 +1,12 @@
+def grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+print(grade(49))       
