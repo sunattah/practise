539884898fc037f57cdf986922238afd0f7f8dc1
sunattah <sundayattah319@gmail.com>
2026-07-21 15:39:53 +0100

    my python list

--- practicefolder/list.py
+++ practicefolder/list.py
@@ -1,6 +1,17 @@
+# def remove_duplicates(items):
+#     itemList = []
+# for x in items:
+#   if x  not in itemList:
+#       itemList.append(x)
+#       return itemList
+
+# print(remove_duplicates([1,3,4,5,5,5,5,6,6,]))
+
 def remove_duplicates(items):
     itemList = []
-for x in items:
-  if x  not in itemList:
-   return itemList
-print(remove_duplicates[1,3,4,5,5,5,5,6,6,])
+    for x in items:
+        if x not in itemList:
+            itemList.append(x)
+    return itemList
+
+print(remove_duplicates([1, 3, 4, 5, 5, 5, 5, 6, 6]))
