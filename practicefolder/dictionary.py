23ba502eacd5ad9ac8bdd32bac20575bfc26a615
sunattah <sundayattah319@gmail.com>
2026-07-23 15:48:51 +0100

    my python file

--- practicefolder/dictionary.py
+++ practicefolder/dictionary.py
@@ -7,8 +7,15 @@
 #         return counts
 # print(remove_duplicates([1, 3, 4, 5, 5, 5, 5, 6, 6]))
 
-letter = ["a", "a", "a", "z", "z", "z", "b", "b", "b", "c"]
+# letter = ["a", "a", "a", "z", "z", "z", "b", "b", "b", "c"]
+# count = {}
+# for i in letter:
+#     count[i] = count.get(i , 0) + 1
+# print(count)
+
+
+sentence = "hello world"
 count = {}
-for i in letter:
-    count[i] = count.get(i , 0) + 1
+for letter in sentence:
+    count[letter] = count.get(letter, 0) + 1
 print(count)
