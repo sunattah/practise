e241b6904941cb04ac9453ce6eb1ba6e06ef37c2
sunattah <sundayattah319@gmail.com>
2026-07-23 14:41:53 +0100

    my dictionary file

--- practicefolder/prac_dic.py
+++ practicefolder/prac_dic.py
@@ -1,10 +1,10 @@
 from numbers import Number
 
 
-num = 1,1,1,3,4,1,4,2,2,2,5,6,7,8,8,9,0
+# num = 1,1,1,3,4,1,4,2,2,2,5,6,7,8,8,9,0
 count = {
     Number : 79
 }
-for x in num:
-    count[x] = count.get(x, 1)
+# for x in num:
+#     count[x] = count.Number
 print(count)
